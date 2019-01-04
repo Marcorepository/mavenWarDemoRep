1ee5a884c64b07d47414fccb2aef0f3bdcdd2f89
Marco <Marco@DESKTOP-I5IHA9U.fritz.box>
2019-01-04 10:44:20 +0100

    App: Worl2

--- src/main/java/de/entwicklerkurs/demo/maven_web_demo/App.java
+++ src/main/java/de/entwicklerkurs/demo/maven_web_demo/App.java
@@ -3,7 +3,7 @@ package de.entwicklerkurs.demo.maven_web_demo;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello Worl2");
 
 	}
 
